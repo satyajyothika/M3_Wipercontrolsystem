c6ce8c2565f27a92b3fe1ca3b9f55b7e343ccbd9
satyajyothika <101032635+satyajyothika@users.noreply.github.com>
2022-05-14 10:57:21 +0530

    Update wiper.h

---
+++ 3_Implementation/inc/wiper.h
@@ -0,0 +1,33 @@
+/*
+ * wiper.h
+ *
+ *  Created on: May 14, 2022
+ *      Author: Satya Jyothika
+ */
+
+#ifndef WIPER_H_
+#define WIPER_H_
+
+
+
+void turnoff_wiper(void);
+
+void turnon_wiper(void);
+
+void mainkey_off(void);
+
+void mainkey_on(void);
+
+void Delay_ms(void);
+
+void motion_direction_clockwise(void);
+
+void motion_direction_anticlockwise(void);
+
+void Reset_Handler(void);
+
+
+
+
+
+#endif /* WIPER_H_ */
